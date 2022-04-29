357b38170316a68acae13a4ae70227dd55980c96
Vinicius pereira de Oliveira <act061@einstein.br>
2022-04-29 19:47:28 -0300

    finish exercise

--- src/ado5/Main.java
+++ src/ado5/Main.java
@@ -1,8 +1,36 @@
 package ado5;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static  void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean isMenuOpen = true;
+        int escolhaUsuario = 0;
 
+        do {
+            System.out.println("Digite: \n1 - Instrucoes\n2 - Jogar\n3 - Creditos\n4 - Sair");
+            escolhaUsuario = scanner.nextInt();
+
+            switch (escolhaUsuario) {
+                case 1:
+                    System.out.println("INSTRUCOES");
+                    break;
+
+                case 2:
+                    System.out.println("Jogar");
+                    break;
+
+                case 3:
+                    System.out.println("CREDITOS");
+                    break;
+
+                case 4:
+                    System.out.println("ATE MAIS");
+                    isMenuOpen = false;
+                    break;
+            }
+        } while(isMenuOpen);
     }
 }
